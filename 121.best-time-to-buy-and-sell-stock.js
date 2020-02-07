98b3f5198dc82aa6291b7770912f6403f8e4149b
Arash Abbasi <s.arash.abbasi@gmail.com>
2020-02-07 13:04:45 -0800

    Add opmtimized solution

--- 121.best-time-to-buy-and-sell-stock.js
+++ 121.best-time-to-buy-and-sell-stock.js
@@ -9,17 +9,31 @@
  * @param {number[]} prices
  * @return {number}
  */
+
+ // Naive solution
+// var maxProfit = function(prices) {
+//   let maxDelta = 0;
+//   for (let i = 0; i < prices.length; i++) {
+//     for (let j = i + 1; j < prices.length; j++) {
+//       if (prices[i] - prices[j] < 0) {
+//         let delta = prices[i] - prices[j];
+//         maxDelta = Math.min(maxDelta, delta);
+//       }
+//     }
+//   }
+//   return -maxDelta;
+// };
+
+// Optimized solution
 var maxProfit = function(prices) {
-  let maxDelta = 0;
+  let min = prices[0];
+  let max = 0;
   for (let i = 0; i < prices.length; i++) {
-    for (let j = i + 1; j < prices.length; j++) {
-      if (prices[i] - prices[j] < 0) {
-        let delta = prices[i] - prices[j];
-        maxDelta = Math.min(maxDelta, delta);
-      }
-    }
+    min = Math.min(min, prices[i]);
+    max = Math.max(max, prices[i] - min);
   }
-  return -maxDelta;
+  return max;
 };
+
 // @lc code=end
 
