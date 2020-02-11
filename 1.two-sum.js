c30037d7b42393ff93f7c1d62e28d07aaaeb9d98
Arash Abbasi <s.arash.abbasi@gmail.com>
2020-02-11 11:04:32 -0800

    Add optimal solution using hash table

--- 1.two-sum.js
+++ 1.two-sum.js
@@ -11,13 +11,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+  let obj = {}
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i+1; j < nums.length; j++) {
-        if (nums[i] + nums[j] === target) {
-            return [i, j]
-        }
-    }
-}
+      const another = target - nums[i];
+      if (another in obj) {
+          return [obj[another], i]
+      }
+      obj[nums[i]] = i;
+  }
 };
 // @lc code=end
 
