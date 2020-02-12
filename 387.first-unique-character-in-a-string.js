6decec7ff1d5188360ff78afbbb3fea035d0f1d3
Arash Abbasi <s.arash.abbasi@gmail.com>
2020-02-11 18:23:06 -0800

    Improve time complexity

--- 387.first-unique-character-in-a-string.js
+++ 387.first-unique-character-in-a-string.js
@@ -10,29 +10,12 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {
-  let charObj = {};
-
-  let res = s.length + 1;
   for (let i = 0; i < s.length; i++) {
-      if (!charObj[s[i]]) {
-          charObj[s[i]] = 1
-      } else {
-          charObj[s[i]]++
-      }
-  }
-
-  for (let key in charObj) {
-     if (charObj[key] > 1) {
-         delete charObj[key];
-     }
-  }
-  if (Object.keys(charObj).length === 0) {
-      return -1;
-  }
-  for (let key in charObj) {
-      res = Math.min(res, s.indexOf(key))
-  }
-  return res;
+    if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
+        return i;
+    }
+}
+ return -1;
 };
 // @lc code=end
 
