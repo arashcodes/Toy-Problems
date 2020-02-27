bfbfe45393cca676c920d02a4fa41cb494aeeb7b
Arash Abbasi <s.arash.abbasi@gmail.com>
2020-02-27 11:26:58 -0800

    Add find method to BST

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -36,6 +36,21 @@ class BinarySearchTree {
       }
     }
   }
+
+  find(value) {
+    if (this.root === null) return false;
+    let current = this.root;
+    while(current) {
+      if (value > current.value) {
+        current = current.right;
+      } else if (value < current.value) {
+        current = current.left;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
 }
 
 
