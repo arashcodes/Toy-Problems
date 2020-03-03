a759e02553a93af6a96850de210220ef0bc5fb03
Arash Abbasi <s.arash.abbasi@gmail.com>
2020-03-02 18:20:48 -0800

    Add solution

---
+++ 14.longest-common-prefix.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=14 lang=javascript
+ *
+ * [14] Longest Common Prefix
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+  if (strs.length === 0) return '';
+  
+  let res = '';
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[0][i] !== strs[j][i]) return res;
+    }
+    res += strs[0][i]
+  }
+  return res;  
+};
+// @lc code=end
+
