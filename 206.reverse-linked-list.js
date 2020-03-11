13ac32330e8d7eebabda7066f7238557f5ad85a8
Arash Abbasi <s.arash.abbasi@gmail.com>
2020-03-10 17:09:24 -0700

    Add solution with iterative approach

---
+++ 206.reverse-linked-list.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=206 lang=javascript
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  let prev = null;
+  let curr = head;
+  while(curr) {
+    const temp = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = temp;
+  }
+  return prev;
+};
+// @lc code=end
+
