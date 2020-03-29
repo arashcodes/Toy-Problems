fcb31b1b4d039fc9ec618ac68e4dab80fa4ed472
Arash Abbasi <s.arash.abbasi@gmail.com>
2020-03-28 17:30:43 -0700

    Add validateBST solution

---
+++ 98.validate-binary-search-tree.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=98 lang=javascript
+ *
+ * [98] Validate Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+  if (!root) return true;
+
+  function validate(node, min, max) {
+    if (!node) return true;
+    if (node.val <= min || node.val >= max) return false;
+    return validate(node.left, min, node.val) && validate (node.right, node.val, max);
+  }
+
+  return validate(root, min=-Infinity, max=Infinity);
+};
+// @lc code=end
+
