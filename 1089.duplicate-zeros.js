497400b8569a0fa452b95838bd1a13f8a6491377
Arash Abbasi <s.arash.abbasi@gmail.com>
2020-05-01 14:43:52 -0700

    Add solution

---
+++ 1089.duplicate-zeros.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=1089 lang=javascript
+ *
+ * [1089] Duplicate Zeros
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+var duplicateZeros = function(arr) {
+  for (var i=0; i<arr.length; i++) {
+    if (arr[i] === 0) {
+      arr.splice(i, 0, 0);
+      arr.pop();
+      i+=1
+    }
+  }
+};
+// @lc code=end
+
