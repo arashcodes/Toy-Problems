fcd2b729ef124ec459ce9fa90367957eb91bf56d
Arash Abbasi <s.arash.abbasi@gmail.com>
2020-05-26 11:59:03 -0700

    Add solution

---
+++ 941.valid-mountain-array.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=941 lang=javascript
+ *
+ * [941] Valid Mountain Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} A
+ * @return {boolean}
+ */
+var validMountainArray = function(A) {
+  if (A.length < 3) return false;
+
+  let left = 0;
+  let right = A.length - 1;
+
+  while (A[left] < A[left + 1]) left++;
+  while (A[right] < A[right - 1]) right--;
+
+  return left === right && left !== 0 && right !== A.length - 1;
+};
+// @lc code=end
+
