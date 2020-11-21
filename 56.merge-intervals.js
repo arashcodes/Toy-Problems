ba7bcf2d2ff249c0a167aee62053b1f0fe9d6172
Arash Abbasi <s.arash.abbasi@gmail.com>
2020-11-21 14:21:27 -0800

    Add cleaner solution

--- 56.merge-intervals.js
+++ 56.merge-intervals.js
@@ -10,21 +10,19 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
-  if (intervals.length <= 1) return intervals;
-  intervals.sort((a, b) => a[0] !== b[0] ? a[0] - b[0] : a[1] - b[1]);
+  intervals.sort((a, b) => a[0] - b[0]);
+  const res = [intervals[0]];
+  let last;
   
-  let prev = intervals[0];
-  const res = [prev];
-
-  for (let curr of intervals) {
-    if (curr[0] <= prev[1]) {
-      prev[1] = Math.max(prev[1], curr[1]);
+  for (let i = 1; i < intervals.length; i += 1) {
+    last = res[res.length - 1];
+    if (last[1] >= intervals[i][0]) {
+      last[1] = Math.max(last[1], intervals[i][1]);
     } else {
-      res.push(curr);
-      prev = curr;
+      res.push(intervals[i]);
     }
   }
-
+  
   return res;
 };
 // @lc code=end
