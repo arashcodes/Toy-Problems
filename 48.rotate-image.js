34fc390e89570e82b1a8b15c1213a9db59adb6a1
Arash Abbasi <s.arash.abbasi@gmail.com>
2020-11-22 15:17:08 -0800

    Change inner loop

--- 48.rotate-image.js
+++ 48.rotate-image.js
@@ -12,8 +12,8 @@
 var rotate = function(matrix) {
   matrix = matrix.reverse();
   for (let i = 0; i < matrix.length; i++) {
-      for (let j = 0; j < i; j++) {
-          [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]]
+    for (let j = i; j < matrix[0].length; j += 1) {
+        [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
       }
   }
   return matrix;
