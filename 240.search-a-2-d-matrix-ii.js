75476aba14f879ab0ea7a29ac49ab7df99f2407c
Arash Abbasi <s.arash.abbasi@gmail.com>
2020-11-21 16:48:14 -0800

    Add solution

---
+++ 240.search-a-2-d-matrix-ii.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=240 lang=javascript
+ *
+ * [240] Search a 2D Matrix II
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+  if (matrix.length === 0) return false;
+
+  let row = 0;
+  let col = matrix[0].length - 1;
+
+  while (row < matrix.length && col >= 0) {
+    if (matrix[row][col] === target) return true;
+
+    else if (matrix[row][col] > target) col -= 1;
+
+    else row += 1;
+  }
+
+  return false;
+};
+// @lc code=end
+
