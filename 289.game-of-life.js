fd8f57b80213afacf36e809eeaa133d006fcb8dc
Arash Abbasi <s.arash.abbasi@gmail.com>
2021-02-06 13:33:05 -0800

    Add solution

--- 289.game-of-life.js
+++ 289.game-of-life.js
@@ -10,15 +10,14 @@
  * @return {void} Do not return anything, modify board in-place instead.
  */
 var gameOfLife = function(board) {
-  const ref = JSON.parse(JSON.stringify(board));
-  for (let i = 0; i < ref.length; i += 1) {
-    for (let j = 0; j < ref[0].length; j += 1) {
+  for (let i = 0; i < board.length; i += 1) {
+    for (let j = 0; j < board[0].length; j += 1) {
       applyRules(i, j);
     }
   }
   
   function applyRules(x, y) {
-    let nextX, nextY, countOnes = 0;
+    let nextX, nextY, liveCounter = 0;
     const dx = [0, 1, 1, 1, 0, -1, -1, -1];
     const dy = [1, 1, 0, -1, -1, -1, 0, 1];
     
@@ -26,17 +25,23 @@ var gameOfLife = function(board) {
       nextX = dx[i] + x;
       nextY = dy[i] + y;
       
-      if (isInRange(nextX, nextY) && ref[nextX][nextY] === 1) countOnes += 1;
+      if (isInRange(nextX, nextY) && Math.abs(board[nextX][nextY]) === 1) liveCounter += 1;
     }
-    // console.log([x, y], countOnes)
     
-    if (ref[x][y] === 1 && countOnes < 2) board[x][y] = 0;
-    if (ref[x][y] === 1 && countOnes > 3) board[x][y] = 0;
-    if (ref[x][y] === 0 && countOnes === 3) board[x][y] = 1;
+    if (board[x][y] === 1 && liveCounter < 2) board[x][y] = -1;
+    if (board[x][y] === 1 && liveCounter > 3) board[x][y] = -1;
+    if (board[x][y] === 0 && liveCounter === 3) board[x][y] = 2;
   }
   
   function isInRange(x, y) {
     return (x >= 0 && x < board.length) && (y >= 0 && y < board[0].length);
+  }
+  
+  for (let i = 0; i < board.length; i += 1) {
+    for (let j = 0; j < board[0].length; j += 1) {
+      if (board[i][j] < 0) board[i][j] = 0;
+      if (board[i][j] > 0) board[i][j] = 1;
+    }
   }
   
   return board;
